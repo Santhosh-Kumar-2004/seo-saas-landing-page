d2d7b27286873e7ed246fba8b62c8e46c4e77fcd
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-09-06 12:52:25 +0530

    Created hr tag with styles

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
     <Features />
     <hr style={{ border: '1px solid rgba(255, 255, 255, 0.3)' }} />
     <WhyHire />
+    <hr style={{ border: '1px solid rgba(255, 255, 255, 0.3)' }} />
       {/* <h1>Welcome to the SEO Page</h1>
       <p>This is a simple landing page optimized for search engines.</p> */}
     </>
