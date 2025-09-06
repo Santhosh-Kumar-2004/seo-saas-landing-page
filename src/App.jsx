c9d0eac9c80be1e91030d9763839b05ff62f39e8
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-09-06 12:10:33 +0530

    Created the Hore me section and aligned items

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
     <Hero />
     <hr style={{ border: '1px solid rgba(255, 255, 255, 0.3)' }} />
     <Features />
+    <hr style={{ border: '1px solid rgba(255, 255, 255, 0.3)' }} />
       {/* <h1>Welcome to the SEO Page</h1>
       <p>This is a simple landing page optimized for search engines.</p> */}
     </>
